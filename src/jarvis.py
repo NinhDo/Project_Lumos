22f522caaaa09e0ab3b15cdcf28542af293d49c5
Ninh Do <doquininh@gmail.com>
2017-02-01 07:44:20 +0100

    Testing: Sends a magic packet to mangostation when it detects the hotword

--- src/jarvis.py
+++ src/jarvis.py
@@ -1,10 +1,45 @@
 # The hotword detector
 import snowboydecoder
+import signal
+# for bash commands
+import subprocess
+
+# For termination
+interrupted = False
+
+# If we signal the program to stop
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+# For termination
+def interrupt_callback():
+    global interrupted
+    return interrupted
 
 # What to do when you hear a word
 def detected_callback():
     print "hotword detected"
+    wake_up()
 
+# Wakes up the mangostation
+def wake_up():
+    subprocess.call("wakeonlan -f ~/mangostation");
 
-detector = snowboydecoder.HotwordDetector("Hey__Jarvis.pmdl", sensitivity=0.5, audio_gain=1)
-detector.start(detected_callback, sleep_time=0.03)
+# Set up the detector, so it looks after "Hey, Jarvis"
+detector = snowboydecoder.HotwordDetector(
+    "Hey__Jarvis.pmdl",
+    sensitivity=0.5,
+    audio_gain=1
+)
+
+def main():
+    # Start listening. Every sleep_time seconds, the function checks if the hotword was detected and calls the callback function, and if we interrupted the program so it can terminate properly
+    detector.start(
+        detected_callback,
+        interrupted_check=interrupt_callback,
+        sleep_time=0.03
+    )
+
+if __name__ == '__main__':
+    main()
